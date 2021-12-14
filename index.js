03fab4baf3f419d30a5815bbdb9e943145dcb1b9
antoninaorobets <pani.antonina.don@gmail.com>
2021-12-14 16:27:27 -0500

    commit tree 03b4e057c591e384ab73adc90816d5a68090e202

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
+  console.log( typeof word)
+   const l = word.length
+   const iter = Math.floor(word.length/2 )
+   let e = 0
+ 
+ 
+   for (let i = 0; i< iter; i++){
+     if (word[i] !== word[l-i-1]) {
+       e = e+1; 
+   }}
+   
+   
+   switch (e === 0) {
+     case true: return true;  break;
+     case false: return false;  break;
+   }
+ }
 
 /* 
-  Add your pseudocode here
+  create revers  string
+  compare
+
+  compare first and last character till the middle:
+  count string length
+  compare a[i] and a[l-i] for l/2 times
 */
 
 /*
@@ -20,6 +40,11 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mood o doom"));
 }
 
 module.exports = isPalindrome;
